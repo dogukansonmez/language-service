4ca8312d4012f2bcfcf235dd84fb7755616c8f0f
dogukansonmez <dogukansonmez@gmail.com>
2014-10-08 16:38:07 +0200

    adding bootstraped to admin

--- italianTurkish/api.py
+++ italianTurkish/api.py
@@ -1,5 +1,7 @@
 from tastypie import fields
+from tastypie.constants import ALL
 from tastypie.resources import ModelResource
+from tastypie.serializers import Serializer
 from italianTurkish.models import Item, Topic
 
 
@@ -8,6 +10,7 @@ class TopicResource(ModelResource):
         queryset = Topic.objects.all()
         resource_name = 'topics'
         excludes = ['resource_uri']
+        serializer = Serializer(formats=['json', 'jsonp'])
 
     def alter_list_data_to_serialize(self, request, data_dict):
         data_dict['topics'] = data_dict['objects']
@@ -24,13 +27,18 @@ class TopicResource(ModelResource):
 
 
 class ItemResource(ModelResource):
-    topic = fields.CharField(attribute="topic")
+    topic = fields.ForeignKey(TopicResource,'topic')
 
     class Meta:
         queryset = Item.objects.all()
         resource_name = 'items'
-        fields = ['source', 'target', 'topic', 'id']
+        fields = ['source', 'target', 'topic','category', 'id']
         excludes = ['resource_uri']
+        filtering = {
+            'topic' : ALL
+        }
+        serializer = Serializer(formats=['json', 'jsonp'])
+
 
     def alter_list_data_to_serialize(self, request, data_dict):
         data_dict['items'] = data_dict['objects']
